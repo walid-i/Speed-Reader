d1f018a1cdf721301cf361a3d9152e6d4e82f38d
Walid Islam <wwalidislam@gmail.com>
2020-07-24 21:16:58 -0400

    Display only shows last word of given sentence

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
+// Function to split string into array of words
+var wordArray;
+function splitWords(text){
+    wordArray = text.split(" ");
+}
 
+$(document).ready(function(){
     $("#startbutton").click(function(){
         text = $("textarea").val();
+        $("#startbutton, #textarea, #paste").fadeOut("3000")
+        $("#wordDisplay").show();
+        splitWords(text);
+        console.log(wordArray);
+        for(var i = 0; i < wordArray.length; i++){
+            console.log(wordArray[i]);
+            document.getElementById("wordDisplay2").innerHTML = wordArray[i];
+        }
     });
 
 });
